c7fd6d09842d5ece276de581e44ecfaabc262ac8
ac ac <iamapythongeek@gmail.com>
2021-04-14 15:50:38 -0400

    added readme

--- quickai/__init__.py
+++ quickai/__init__.py
@@ -1,3 +1,4 @@
 from .image_classification import ImageClassification, ImageClassificationPredictor
 from .text_inferance import gpt_neo, q_and_a, sentiment_analysis, mask_lang, ner, summarization
+from .text_finetuning import TextFineTuning
 
