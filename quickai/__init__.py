e93d4b4e471a567df9eb52d7271ccb6576901600
ac ac <iamapythongeek@gmail.com>
2021-04-17 11:55:29 -0400

    Added classification inferance

--- quickai/__init__.py
+++ quickai/__init__.py
@@ -1,3 +1,3 @@
 from .image_classification import ImageClassification, ImageClassificationPredictor
-from .text_inferance import gpt_neo, q_and_a, sentiment_analysis, ner, summarization
+from .text_inferance import gpt_neo, q_and_a, sentiment_analysis, ner, summarization, classification_ft
 from .text_finetuning import TextFineTuning
