f7fc8ceb7ff376c0d087c526d6782e961d28b29b
ac ac <iamapythongeek@gmail.com>
2021-04-29 20:23:33 -0400

    Jupyter

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="quickai",  # Replace with your own username
-    version="1.0.3",
+    version="1.1.0",
     author="geekjr",
     author_email="author@example.com",
     description="QuickAI is a Python library that makes it extremely easy to experiment with state-of-the-art "
