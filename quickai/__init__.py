05dc377c8abd41499500c201e78377a0990f7b0f
geekjr <58124406+geekjr@users.noreply.github.com>
2021-05-05 14:57:48 -0400

    Create __init__.py

---
+++ quickai/__init__.py
@@ -0,0 +1,7 @@
+"""
+from quickai import *
+"""
+
+from .image_classification import ImageClassification, ImageClassificationPredictor
+from .text_inferance import gpt_neo, q_and_a, sentiment_analysis, ner, summarization, classification_ft
+from .text_finetuning import TextFineTuning
