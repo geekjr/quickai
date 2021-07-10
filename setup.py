694b7fedc88d4767c05dc78bd222280a8eca0d43
geekjr <58124406+geekjr@users.noreply.github.com>
2021-07-10 18:04:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,8 +21,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=["quickai", "yolo"],
-    #packages=setuptools.find_packages(),
+    packages=["quickai/"],
     python_requires=">=3.6",
     install_requires=[
         'scikit-learn',
