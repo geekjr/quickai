e4738606a7e6ccce6fdecad2d0c8987a9b0ae89f
geekjr <iamapythongeek@gmail.com>
2021-07-10 19:16:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="quickai",  # Replace with your own username
-    version="1.3.5",
+    version="1.3.6",
     author="geekjr",
     author_email="author@example.com",
     description="QuickAI is a Python library that makes it extremely easy to experiment with state-of-the-art "
@@ -21,7 +21,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=[".quickai"],
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires=[
         'scikit-learn',
